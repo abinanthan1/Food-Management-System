d2e3d7f5f2d988fba44f39659e50270e20c0b949
abinanthan1 <abinanthan1011@gmail.com>
2025-05-30 16:16:53 +0530

    Created OrderAPI Controller with product service in the server side and Implemented it in client side

--- Mango.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
+++ Mango.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
@@ -1,7 +1,6 @@
 ﻿
-using System.ComponentModel.DataAnnotations;
 
-namespace Mango.Services.ShoppingCartAPI.Models.Dto
+namespace Mango.Services.OrderAPI.Models.Dto
 {
     public class CartHeaderDto
     {
@@ -10,11 +9,8 @@ namespace Mango.Services.ShoppingCartAPI.Models.Dto
         public string? CouponCode { get; set; }
         public double Discount { get; set; }
         public double CartTotal { get; set; }
-        [Required]
         public string? Name { get; set; }
-        [Required]
         public string? Email { get; set; }
-        [Required]
         public string? Phone { get; set; }
     }
 }
